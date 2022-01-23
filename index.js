339db58bf640d3ebfb7b253b4726ec70643ec51e
Kova <frits1087@gmail.com>
2022-01-24 01:16:48 +0200

    Home work

---
+++ index.js
@@ -0,0 +1,57 @@
+class RangeValidator {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    set from(value){
+        if(typeof value !== 'number'){
+            throw new TypeError('Parameters has unappropriate type');
+        }
+        this._from = value;
+    }
+
+    get from(){
+        return this._from;
+    }
+
+    set to(value){
+        if(typeof value !== 'number'){
+            throw new TypeError('Parameters has unappropriate type');
+        }
+
+        if(value <= this.from){
+            throw new RangeError('Not correct value count');
+        }
+        this._to = value;
+    }
+
+    get to(){
+        return this._to;
+    }
+
+    get range(){
+        const wholeNumbers = [];
+        for (let i = this._from; i <= this._to; i++) {
+           wholeNumbers.push(i);
+        }
+        return wholeNumbers;
+    }
+
+    validate(value) {
+        for (let i = this._from; i <= this._to; i++) {
+            if(value >= this._from && value <= this._to){
+            alert('такое число есть');
+            break;
+            } else {
+              throw new RangeError('Это число не входит в заданный диапазон');
+  
+            }
+        }
+    }
+}
+
+
+
+
+
